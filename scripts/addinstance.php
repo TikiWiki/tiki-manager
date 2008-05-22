3d8fd277886117ee7caea520b3364039ad5d2ba5
lphuberdeau <lphuberdeau@users.sourceforge.net>
2008-05-22 16:10:02 +0000

    [MOD] Change the order in which information is requested to have better defaults
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trim@12940 b456876b-0849-0410-b77d-98878d47e9d5

--- scripts/addinstance.php
+++ scripts/addinstance.php
@@ -4,34 +4,48 @@
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
 include dirname(__FILE__) . "/../src/env_setup.php";
-$name = $contact = $webroot = $tempdir = $weburl = '';
-
-while( empty( $name ) )
-	$name = readline( "Instance name : " );
-while( empty( $contact ) )
-	$contact = readline( "Contact email : " );
-while( empty( $webroot ) )
-	$webroot = readline( "Web root : " );
-while( empty( $weburl ) )
-	$weburl = readline( "Web URL : " );
-while( empty( $tempdir ) )
-	$tempdir = readline( "Working directory : " );
-
-$instance = new Instance;
-$instance->name = $name;
-$instance->contact = $contact;
-$instance->webroot = $webroot;
-$instance->weburl = $weburl;
-$instance->tempdir = $tempdir;
-
-$instance->save();
-echo "Instance information saved.\n";
 
 $user = $host = '';
 while( empty( $host ) )
 	$host = readline( "SSH host name : " );
 while( empty( $user ) )
 	$user = readline( "SSH user : " );
+
+$name = $contact = $webroot = $tempdir = $weburl = '';
+
+$d_name = $host;
+$d_webroot = "/home/$user/public_html";
+$d_tempdir = "/home/$user/trim_temp";
+$d_weburl = "http://$host";
+
+$name = readline( "Instance name : [$d_name] " );
+if( empty( $name ) )
+	$name = $d_name;
+
+while( empty( $contact ) )
+	$contact = readline( "Contact email : " );
+
+$webroot = readline( "Web root : [$d_webroot] " );
+if( empty( $webroot ) )
+	$webroot = $d_webroot;
+
+$weburl = readline( "Web URL : [$d_weburl] " );
+if( empty( $weburl ) )
+	$weburl = $d_weburl;
+
+$tempdir = readline( "Working directory : [$d_tempdir] " );
+if( empty( $tempdir ) )
+	$tempdir = $d_tempdir;
+
+$instance = new Instance;
+$instance->name = $name;
+$instance->contact = $contact;
+$instance->webroot = rtrim( $webroot, '/' );
+$instance->weburl = rtrim( $weburl, '/' );
+$instance->tempdir = rtrim( $tempdir, '/' );
+
+$instance->save();
+echo "Instance information saved.\n";
 
 $access = $instance->registerAccessMethod( 'ssh', $host, $user );
 
@@ -40,6 +54,11 @@ if( ! $access )
 	$instance->delete();
 	echo "Set-up failure. Instance removed.\n";
 }
+
+if( $access instanceof ShellPrompt )
+	$access->shellExec( "mkdir -p $tempdir" );
+else
+	echo "Shell access is required to create the working directory. You will need to create it manually.\n";
 
 if( ! $instance->detectPHP() )
 	die( "PHP Interpreter could not be found on remote host.\n" );
