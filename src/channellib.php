8d1ea9af522fcc83a2a25ea03980c8e982d2fa80
lphuberdeau <lphuberdeau@users.sourceforge.net>
2009-07-16 14:48:28 +0000

    [NEW] Library to call data channels
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trim@20181 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ src/channellib.php
@@ -0,0 +1,46 @@
+<?php
+
+class Channel
+{
+	private $url;
+	private $user;
+	private $pass;
+
+	private $channels = array();
+
+	function __construct( $channelHandlerUrl ) {
+		$this->url = $channelHandlerUrl;
+	}
+
+	function setAuthentication( $user, $password ) {
+		$this->user = $user;
+		$this->pass = $password;
+	}
+
+	function push( $channelName, $data ) {
+		$this->channels[] = array_merge( $data, array( 'channel_name' => $channelName ) );
+	}
+
+	function process() {
+		$header = "Content-Type: application/x-www-form-urlencoded\r\n";
+		if( $this->user && $this->pass ) {
+			$encoded = base64_encode( $this->user . ':' . $this->pass );
+			$header .= "Authorization: Basic $encoded\r\n";
+		}
+
+		$content = http_build_query( array( 'channels' => $this->channels ), '', '&' );
+
+		$context = stream_context_create( array(
+			'http' => array(
+				'method' => 'POST',
+				'header' => $header,
+				'content' => $content,
+				'timeout' => 10,
+			),
+		) );
+
+		file_get_contents( $this->url, false, $context );
+	}
+}
+
+?>
