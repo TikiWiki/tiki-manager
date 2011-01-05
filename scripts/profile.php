8f2bbdeedddc1a8ed13bcccf1ed3a798a0c35c0d
marclaporte <marclaporte@users.sourceforge.net>
2011-01-05 00:37:22 +0000

    [FIX] terminology: applying a profile (and not installing a profile)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trim@31766 b456876b-0849-0410-b77d-98878d47e9d5

--- scripts/profile.php
+++ scripts/profile.php
@@ -8,7 +8,7 @@ include_once dirname(__FILE__) . "/../src/clean.php";
 
 $instances = Instance::getInstances();
 
-echo "Which instances do you want to install a profile on?\n";
+echo "Which instances do you want to apply a profile on?\n";
 
 foreach( $instances as $key => $i )
 	echo "[$key] " . str_pad( $i->name, 20 ) . str_pad( $i->weburl, 30 ) . str_pad( $i->contact, 20 ) . "\n";
@@ -24,7 +24,7 @@ while( ! $profile = trim( readline( 'Profile: ' ) ) );
 
 foreach( $selection as $instance )
 {
-	info( "Installing profile on {$instance->name}" );
+	info( "Applying profile on {$instance->name}" );
 	$instance->getApplication()->installProfile( $repository, $profile );
 }
 
