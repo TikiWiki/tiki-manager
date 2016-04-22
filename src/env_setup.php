558917f22074d07435f21c444ba751ef3ea7db0e
dlucio <dlucio@users.sourceforge.net>
2016-04-22 14:37:04 +0000

    harmless slash
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trim@58409 b456876b-0849-0410-b77d-98878d47e9d5

--- src/env_setup.php
+++ src/env_setup.php
@@ -1,4 +1,4 @@
-/<?php
+<?php
 // Copyright (c) 2008, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
