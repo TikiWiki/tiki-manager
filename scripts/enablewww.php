c281b1b70c4ca8bb39a45d7347ca33fd3ba94640
dlucio <dlucio@users.sourceforge.net>
2016-05-13 03:34:53 +0000

    [FIX] WEBTrim is back
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trim@58593 b456876b-0849-0410-b77d-98878d47e9d5

--- scripts/enablewww.php
+++ scripts/enablewww.php
@@ -1,4 +1,18 @@
 <?php
+if (function_exists(posix_getuid)){
+	if (posix_getuid() == 0){
+		echo "Good! This is root!\n";
+	} 
+	else {
+	        echo "This is non-root";
+		echo "You need to execute this script as root, it will write some configuration files";
+		exit(1);
+	}
+}
+else {
+	echo "PHP POSIX functions are not installed, install them and try again\n";
+	exit(1);
+}
 
 include_once dirname(__FILE__) . "/../src/env_setup.php";
 
@@ -25,10 +39,30 @@ echo "This will enable the TRIM administration web panel.\n";
 if( 'confirm' != readline( "Type 'confirm' to continue: " ) )
 	exit;
 
-$d_folder = "/var/www/webtrim";
-$folder = readline( "TRIM location : [$d_folder] " );
-if( empty( $folder ) )
-	$folder = $d_folder;
+$ret_var = -1;
+$out = '/etc/httpd/';
+$cmd = 'dirname $(find /etc/httpd -name httpd.conf )';
+exec ($cmd, $out, $ret_var);
+$out2 = $out[0];
+
+$d_httpd = readline( "Apache httpd.conf directory : [$out2] " );
+if( empty( $d_httpd ) )
+	$d_httpd = $out2;
+
+$base = dirname($d_httpd);
+$precmd = 'dirname $(find '.$base.' -name httpd.conf -exec grep ^Include {} \; | cut -d'."' '".' -f2)|sort|head -n1';
+$cmd = $precmd;
+exec ($cmd, $out, $ret_var);
+
+$out2 = $base.'/'.$out[1];
+$d_httpd_optional = readline( "Apache httpd.conf IncludeOptional directory : [$out2] " );
+if( empty( $d_httpd_optional ) )
+	$d_httpd_optional = $out2;
+
+//$d_folder = "/var/www/webtrim";
+//$folder = readline( "TRIM location : [$d_folder] " );
+//if( empty( $folder ) )
+//	$folder = $d_folder;
 
 $user = $pass = '';
 while( empty( $user ) )
@@ -39,7 +73,7 @@ while( empty( $pass ) ) {
 	$pass = getPassword(true); print "\n";
 }
 
-$d_restrict = 'yes';
+$d_restrict = 'no';
 $restrict = readline( "Restrict use to localhost : [$d_restrict] " );
 if( empty( $restrict ) )
 	$restrict = $d_restrict;
@@ -62,10 +96,36 @@ define( 'PASSWORD', '$pass' );
 define( 'RESTRICT', $restrict );
 CONFIG
 );
+
 $web = realpath( dirname(__FILE__) . '/../www' );
+
+file_put_contents ( $d_httpd_optional.'/webtrim.conf', <<<CONFIG
+Alias /webtrim $web
+
+<Directory $web/>
+LogLevel alert rewrite:trace6
+
+AllowOverride All
+<IfModule mod_authz_core.c>
+        # Apache 2.4
+       Require all granted
+</IfModule>
+<IfModule !mod_authz_core.c>
+        # Apache 2.2
+        order deny,allow
+        allow from all
+</IfModule>
+</Directory>
+CONFIG
+);
 
 $db = DB_FILE;
 $data = dirname( DB_FILE );
-`$prefix ln -sf $web $folder`;
+#`$prefix ln -sf $web $folder`;
 `$prefix chmod 0666 $db`;
 `$prefix chmod 0777 $data`;
+
+echo "Please restart your apache before continuing\n";
+echo "WWW Trim is now enabled\n";
+echo "Go to your browser and do http://server_ip/webtrim/\n";
+echo "Enjoy!\n";
