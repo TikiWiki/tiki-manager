30e96fc7972516147f00fba281e4238714eb7135
sokoloski <sokoloski@users.sourceforge.net>
2016-11-01 18:06:38 +0000

    [ENH] src/getlinuxdistro.php: Now has ability to run regex on release file contents.  Fix for Ubuntu being detected as Debian. [#4943]
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trim@60113 b456876b-0849-0410-b77d-98878d47e9d5

--- src/getlinuxdistro.php
+++ src/getlinuxdistro.php
@@ -1,39 +1,30 @@
 function getLinuxDistro()
 {
-    // Declare Linux distros(extensible list).
+    // Declare Linux distros(extensible list).  Order is important (see Ubuntu).
     $distros = array(
-        "Arch" => "arch-release",
-        "Debian" => "debian_version",
-        "Fedora" => "fedora-release",
-        "ClearOS" => "clearos-release",
-        "CentOS" => "centos-release",
-        "Mageia" => "mageia-release",
-        "Redhat" => "redhat-release"
+        "Arch" => array("release" => "arch-release", "regex" => null),
+        "Ubuntu" => array("release" => "issue", "regex" => "/^Ubuntu/"),
+        "Debian" => array("release" => "debian_version", "regex" => null),
+        "Fedora" => array("release" => "fedora-release", "regex" => null),
+        "ClearOS" => array("release" => "clearos-release", "regex" => null),
+        "CentOS" => array("release" => "centos-release", "regex" => null),
+        "Mageia" => array("release" => "mageia-release", "regex" => null),
+        "Redhat" => array("release" => "redhat-release", "regex" => null)
     );
 
-    // Get everything from /etc directory.
-    $etcList = scandir("/etc");
-
-    // Loop through /etc results...
-    // $OSDistro;
-    foreach ($distros as $distroReleaseFile)
-    {
-        // Loop through list of distros..
-        foreach ($etcList as $entry)
-        {
-            // Match was found.
-            if ($distroReleaseFile === $entry)
-            {
-                // Find distros array key(i.e. Distro name) by value(i.e. distro release file)
-                $OSDistro = array_search($distroReleaseFile, $distros);
-
-                break 2; // Break inner and outer loop.
-            }
+    // Iterate over distros array, check if release file exists.
+    // Optionally, run regex on contents.
+    foreach ($distros as $distro => $info) {
+        if (! file_exists("/etc/{$info["release"]}")) continue;
+        if ($info["regex"] != null) {
+            $contents = file_get_contents("/etc/{$info["release"]}");
+            if (! preg_match($info["regex"], $contents)) continue;
         }
+
+        return $distro;
     }
 
-    return $OSDistro;
-
+    return "Unknown";
 }
 
 echo getLinuxDistro();
