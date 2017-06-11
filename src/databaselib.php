6024057352462ff61c60fccb7b90a56ce1463a47
rjsmelo <rjsmelo@users.sourceforge.net>
2017-06-11 13:53:07 +0000

    [FIX] In PHP >= 7.0 mysql_escape_string do not exists
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trim@62961 b456876b-0849-0410-b77d-98878d47e9d5

--- src/databaselib.php
+++ src/databaselib.php
@@ -131,8 +131,8 @@ class Database_Adapter_Mysql implements Database_Adapter
     function createUser(Instance $instance, $username, $password) // {{{
     {
         // FIXME : Not FTP compatible
-        $u = mysql_escape_string($username);
-        $p = mysql_escape_string($password);
+        $u = $this->escapeMysqlString($username);
+        $p = $this->escapeMysqlString($password);
         $query = escapeshellarg("CREATE USER '$u'@'{$this->host}' IDENTIFIED BY '$p';");
 
         $access = $instance->getBestAccess('scripting');
@@ -142,8 +142,8 @@ class Database_Adapter_Mysql implements Database_Adapter
     function grantRights(Instance $instance, $username, $database) // {{{
     {
         // FIXME : Not FTP compatible
-        $u = mysql_escape_string($username);
-        $d = mysql_escape_string($database);
+        $u = $this->escapeMysqlString($username);
+        $d = $this->escapeMysqlString($database);
         $query = escapeshellarg("GRANT ALL ON `$d`.* TO '$u'@'{$this->host}';");
 
         $access = $instance->getBestAccess('scripting');
@@ -161,6 +161,22 @@ class Database_Adapter_Mysql implements Database_Adapter
     {
         return array('mysqli', 'mysql', 'pdo_mysql');
     } // }}}
+
+    protected function escapeMysqlString($string)
+    {
+        if (function_exists('mysql_escape_string')){
+            return mysql_escape_string($string);
+        }
+
+        // Fallback for new versions of PHP where DB connection is required
+        // See:
+        // http://php.net/manual/en/function.mysql-real-escape-string.php#101248
+        // https://dev.mysql.com/doc/refman/5.5/en/mysql-real-escape-string.html
+        // https://github.com/mysql/mysql-server/blob/71f48ab393bce80a59e5a2e498cd1f46f6b43f9a/mysys/charset.c
+        $search = array("\\",  "\x00", "\n",  "\r",  "'",  '"', "\x1a");
+        $replace = array("\\\\","\\0","\\n", "\\r", "\\'", '\"', "\\Z");
+        return str_replace($search, $replace, $string);
+    }
 }
 
 // vi: expandtab shiftwidth=4 softtabstop=4 tabstop=4
