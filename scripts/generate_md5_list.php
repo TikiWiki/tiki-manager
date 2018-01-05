5c533eab76b068ad417323b9eff92a3c45bce2e4
montefuscolo <montefuscolo@users.sourceforge.net>
2018-01-05 18:26:04 +0000

    [ENH] Use RecursiveDirectoryIterator to read files and generate md5 hash
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trim@65094 b456876b-0849-0410-b77d-98878d47e9d5

--- scripts/generate_md5_list.php
+++ scripts/generate_md5_list.php
@@ -4,25 +4,6 @@
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
-if (! function_exists('scandir')) {
-    function scandir($dir, $sortorder = 0)
-    {
-        if (is_dir($dir)) {
-            $dirlist = opendir($dir);
-
-            while (($file = readdir($dirlist)) !== false)
-                $files[] = $file;
-
-            // arsort was replaced with rsort
-            ($sortorder == 0) ? asort($files) : rsort($files);
-
-            return $files;
-        }
-        else
-            return true;
-    }
-}
-
 $cur = getcwd();
 
 if (array_key_exists('REQUEST_METHOD', $_SERVER))
@@ -35,39 +16,14 @@ elseif (count($_SERVER['argv']) > 1)
 if (file_exists($next))
     chdir($next);
 
-if (! function_exists('md5_file_recurse')) {
-    function md5_file_recurse($location, &$fulllist)
-    {
-        $files = scandir($location);
-        foreach ($files as $child) {
-            if (in_array($child, array('.', '..', 'CVS', '.svn')))
-                continue;
+$diriterator = new RecursiveDirectoryIterator('.');
+$objiterator = new RecursiveIteratorIterator($diriterator, RecursiveIteratorIterator::SELF_FIRST);
 
-            $full = "$location/$child";
-            $full = realpath($full);
-
-            if (array_key_exists($full, $fulllist))
-                continue;
-
-            if (is_dir($full))
-                md5_file_recurse($full, $fulllist);
-
-            if (in_array(substr($child, -4),
-                array('.php', '.tpl')) && is_readable($full))
-                $fulllist[$full] = md5_file($full);
-        }
+foreach($objiterator as $name => $object) {
+    if ($object->getType() === 'file' && is_readable($name)) {
+        printf("%s:%s\n", md5_file($name), $name);
     }
 }
 
-$list = array();
-md5_file_recurse('.', $list);
-
-$root = getcwd();
-foreach ($list as $file => $hash) {
-    $file = str_replace($root, '.', $file);
-    echo "$hash:$file\n";
-}
-
 chdir($cur);
-
 // vi: expandtab shiftwidth=4 softtabstop=4 tabstop=4
