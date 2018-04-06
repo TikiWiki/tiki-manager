31f9825f00f7fcf0da40f05f2607aac3575430d8
montefuscolo <montefuscolo@users.sourceforge.net>
2018-04-06 22:25:36 +0000

    [FIX] Avoid to use Tiki shell script to migrate database.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trim@65948 b456876b-0849-0410-b77d-98878d47e9d5

--- scripts/tiki/sqlupgrade.php
+++ scripts/tiki/sqlupgrade.php
@@ -10,8 +10,8 @@ if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'GET')
 if ($root = $_SERVER['argv'][1])
     chdir($root);
 
+$local_php = 'db/local.php';
 if (file_exists('installer/shell.php') && !file_exists('console.php')) {
-    $local_php = 'db/local.php';
 
     require_once('installer/installlib.php');
     include_once('lib/adodb/adodb.inc.php');
@@ -23,7 +23,35 @@ if (file_exists('installer/shell.php') && !file_exists('console.php')) {
     $installer = new Installer;
     $installer->update();
 }
-else
-    `sh doc/devtools/sqlupgrade.sh`;
+else {
+    if(!is_dir('db')) {
+        fwrite(STDERR, getcwd() . "/db/ directory not found");
+        exit(1);
+    }
+    $command = PHP_BINARY . '-d memory_limit=256M console.php -n database:update';
+
+    $match = null;
+    $db_files = glob('db/*local.php');
+    foreach ($db_files as $file) {
+        $retval = -1;
+        $retstr = '';
+        $args = '';
+
+        if (! preg_match(',^db/([a-z0-9_.-]+)?local.php$,', $file, $match)) {
+            continue;
+        }
+
+        if(count($match) === 3) {
+            $args .=  " --site=" . $match[2];
+        }
+
+        $retstr = system($command . $args, $retval);
+        if ($retval === 0) {
+            echo $retstr;
+        } else {
+            fwrite(STDERR, $retstr . "\n");
+        }
+    }
+}
 
 // vi: expandtab shiftwidth=4 softtabstop=4 tabstop=4
