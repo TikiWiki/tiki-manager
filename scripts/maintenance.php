b4c36672483c23f8a2f3efa66d2ddf856a06ed58
montefuscolo <montefuscolo@users.sourceforge.net>
2018-05-15 14:00:35 +0000

    [ENH] Show maintenance start date
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trim@66379 b456876b-0849-0410-b77d-98878d47e9d5

--- scripts/maintenance.php
+++ scripts/maintenance.php
@@ -6,16 +6,37 @@
 
 header("HTTP/1.1 503 Service Unavailable", true, 503);
 header("Retry-After: 3600");
+$start_date = getdate(filemtime(__FILE__));
 
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
- <head>
- <title>Maintenance in progress</title>
- </head>
- <body>
-  <h1>Maintenance in progress</h1>
-  <p>An update is currently in progress on this website. Please try again in a few minutes.</p>
- </body>
+    <head>
+        <title>Maintenance in progress</title>
+        <script type="text/javascript">
+            window.trim = window.trim || {};
+            trim.start_date = new Date(<?=
+                $start_date['year']
+                . ',' . $start_date['mon']
+                . ',' . $start_date['mday']
+                . ',' . $start_date['hours']
+                . ',' . $start_date['minutes']
+                . ',' . $start_date['seconds']
+            ;?>);
+
+            function start_date(date) {
+                date = date || trim.start_date;
+                return date.toLocaleString();
+            }
+        </script>
+    </head>
+    <body>
+        <h1>Maintenance in progress</h1>
+        <p>
+            An update started on <b><script type="text/javascript">
+                document.write(start_date());
+            </script></b> and it is currently in progress on this website.
+            Please try again in a few minutes.
+        </p>
+    </body>
 </html>
