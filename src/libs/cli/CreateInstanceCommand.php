a6fae6c8977a343850215f5362b0e9b5292ebabd
montefuscolo <montefuscolo@users.sourceforge.net>
2018-06-19 19:37:01 +0000

    [NEW] Creating a new TRIM cli based on Symfony "console" component
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trim@66763 b456876b-0849-0410-b77d-98878d47e9d5

--- src/libs/cli/CreateInstanceCommand.php
+++ src/libs/cli/CreateInstanceCommand.php
@@ -2,12 +2,14 @@
 
 namespace trim\cli\command;
 
+use trim\instance\Discovery;
 use \Symfony\Component\Console\Command\Command;
-use \Symfony\Component\Console\Input\InputInterface;
-use \Symfony\Component\Console\Output\OutputInterface;
+use \Symfony\Component\Console\Exception\InvalidArgumentException;
 use \Symfony\Component\Console\Input\InputArgument;
+use \Symfony\Component\Console\Input\InputInterface;
 use \Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Question\Question;
+use \Symfony\Component\Console\Output\OutputInterface;
+use \Symfony\Component\Console\Question\Question;
 
 class CreateInstanceCommand extends Command
 {
@@ -17,42 +19,102 @@ class CreateInstanceCommand extends Command
             ->setName('instance:create')
             ->setDescription('Creates a new Instance.')
             ->addArgument('name', InputArgument::REQUIRED, 'Instance name')
-            ->addArgument('contact', InputArgument::REQUIRED, 'Responsible e-mail')
             ->addArgument('webroot', InputArgument::REQUIRED, 'Absolute path to instance folder')
             ->addArgument('weburl', InputArgument::REQUIRED, 'Instance URL')
             ->addArgument('type', InputArgument::REQUIRED, 'Connection type, one of ['.join(', ', \Instance::TYPES).']')
             ;
 
         $this
-            ->addOption('blank', null, InputOption::VALUE_NONE, 'Blank instance')
-            ->addOption('phpexec', null, InputOption::VALUE_REQUIRED, 'PHP binary path', PHP_BINARY)
-            ->addOption('tempdir', null, InputOption::VALUE_REQUIRED, 'Temporary folder', TRIM_TEMP)
-            ->addOption('host', null, InputOption::VALUE_REQUIRED, 'The host address', null)
-            ->addOption('user', null, InputOption::VALUE_REQUIRED, 'The user, used to connect on remote host', null)
-            ->addOption('port', null, InputOption::VALUE_REQUIRED, 'The port, used to connect on remote host', null)
-            ->addOption('password', null, InputOption::VALUE_REQUIRED, 'A password, used to connect on remote host', null)
-            ->addOption('backup_user', null, InputOption::VALUE_REQUIRED, 'The owner for backup files', null)
             ->addOption('backup_group', null, InputOption::VALUE_REQUIRED, 'The group for backup files', null)
-            ->addOption('backup_perm', null, InputOption::VALUE_REQUIRED, 'The octal permission for backup files', '0750')
+            ->addOption('backup_perm', null, InputOption::VALUE_REQUIRED, 'The octal permission for backup files', null)
+            ->addOption('backup_user', null, InputOption::VALUE_REQUIRED, 'The owner for backup files', null)
+            ->addOption('blank', null, InputOption::VALUE_NONE, 'Blank instance')
+            ->addOption('contact', null, InputOption::VALUE_REQUIRED, 'Responsible e-mail', null)
+            ->addOption('host', null, InputOption::VALUE_REQUIRED, 'The host address', null)
+            ->addOption('password', null, InputOption::VALUE_REQUIRED, 'A password, used to connect on remote host', null)
+            ->addOption('phpexec', null, InputOption::VALUE_REQUIRED, 'PHP binary path', null)
+            ->addOption('port', null, InputOption::VALUE_REQUIRED, 'The port, used to connect on remote host', null)
+            ->addOption('tempdir', null, InputOption::VALUE_REQUIRED, 'Temporary folder', TRIM_TEMP)
+            ->addOption('user', null, InputOption::VALUE_REQUIRED, 'The user, used to connect on remote host', null)
             ;
+    }
+
+    protected function check(InputInterface $input, OutputInterface $output)
+    {
+        $self = $this;
+        $definition = $this->getDefinition();
+        $fields = array_merge(
+            array_keys($definition->getArguments()),
+            array_keys($definition->getOptions())
+        );
+        $errors = array();
+        foreach ($fields as $field) {
+            $method = 'check' . ucfirst($field);
+            $valid = (
+                ! method_exists($this, $method)
+                || $this->{$method}($input, $output)
+            );
+        }
+        return true;
+    }
+
+    protected function checkType($input, $output)
+    {
+        $type = $input->getArgument('type');
+        $required = array(
+            'local' => array(),
+            'ftp' => array('user', 'host', 'password'),
+            'ssh' => array('user', 'host')
+        );
+
+        if(isset($required[$type])) {
+            $options = array_map(array($input, 'getOption'), $required[$type]);
+            $options = array_combine($required[$type], $options);
+        } else {
+            throw new InvalidArgumentException("Type '$type' is unknown.");
+        }
+
+        $callback = function($v, $o) { return $v && strlen($o) > 0; };
+        $valid = array_reduce($options, $callback, true);
+
+        if (!$valid) {
+            $options = array_map('escapeshellarg', $required[$type]);
+            throw new InvalidArgumentException(
+                "For type '$type', the options [" . join(', ', $options)
+                ."] are required"
+            );
+        }
+
+        return $valid;
+    }
+
+    protected function checkHost($input, $output)
+    {
+        $type = $input->getArgument('type');
+        if($type === 'local') {
+            return true;
+        }
+        $host = $input->getOption('host');
+        $port = $input->getOption('port') ?: ($type === 'ssh' ? 22 : 21);
+        $sock = fsockopen($host, $port);
+        $valid = is_resource($sock);
+        fclose($sock);
+        return $valid;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->check($input, $output);
         $blank = $input->getOption('blank');
 
         $instance = new \Instance();
         $instance->name = $input->getArgument('name');
-        $instance->contact = $input->getArgument('contact');
         $instance->webroot = $input->getArgument('webroot');
         $instance->weburl = $input->getArgument('weburl');
         $instance->type = $input->getArgument('type');
 
-        $instance->phpexec = $input->getOption('phpexec');
+        $instance->contact = $input->getOption('contact');
         $instance->tempdir = $input->getOption('tempdir');
-        $instance->backup_user = $input->getOption('backup_user');
-        $instance->backup_group = $input->getOption('backup_group');
-        $instance->backup_perm = $input->getOption('backup_perm');
         $instance->save();
 
         $access = $instance->registerAccessMethod(
@@ -62,5 +124,28 @@ class CreateInstanceCommand extends Command
             $input->getOption('password'),
             $input->getOption('port')
         );
+
+        $discovery = new Discovery($instance, $access);
+
+        $instance->phpexec = $input->getOption('phpexec');
+        if(!$instance->phpexec) {
+            $instance->phpexec = $discovery->detectPHP();
+            $output->writeln("Detected PHP binary: {$instance->phpexec}");
+
+            $instance->phpversion = $discovery->detectPHPVersion();
+            $output->writeln("Detected PHP version: {$instance->phpversion}");
+        }
+
+        $backup_perm = $discovery->detectBackupPerm();
+        $instance->backup_user = $input->getOption('backup_user') ?: $backup_perm[0];
+        $instance->backup_group = $input->getOption('backup_group') ?: $backup_perm[1];
+        $instance->backup_perm = intval($input->getOption('backup_perm'), 8) ?: $backup_perm[2];
+        $instance->save();
+
+        if($blank) {
+            exit(0);
+        }
+
+        perform_instance_installation($instance);
     }
 }
