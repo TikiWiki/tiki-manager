52a900c56dcd3e1db805feb399aec4023ec170ff
montefuscolo <montefuscolo@users.sourceforge.net>
2018-06-19 22:11:11 +0000

    [ENH] Refactor addinstance script to use new Discovery class
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trim@66768 b456876b-0849-0410-b77d-98878d47e9d5

--- scripts/addinstance.php
+++ scripts/addinstance.php
@@ -11,130 +11,51 @@ define('ARG_BLANK', $_SERVER['argc'] == 2 && $_SERVER['argv'][1] == 'blank');
 
 echo color("\nAnswer the following to add a new TRIM instance.\n\n", 'yellow');
 
-$type = $user = $host = $pass = '';
+$instance = new Instance();
+$instance->type = strtolower(promptUser('Connection type ', null, Instance::TYPES));
 
-$type = strtolower(
-    promptUser('Connection type', null, array('ftp', 'local', 'ssh'))
-);
+$access = Access::getClassFor($instance->type);
+$access = new $access($instance);
+$discovery = new \trim\instance\Discovery($instance, $access);
 
-if ($type != 'local') {
-    $host = promptUser('Host name');
-    $port = promptUser('Port number', ($type == 'ssh') ? 22 : 21);
-    $user = strtolower(promptUser('User'));
-
-    while ($type == 'ftp' && empty($pass)) {
-        print "Password : ";
-        $pass = getPassword(true); print "\n";
-    }
-
-    $d_name = $host;
-}
-else if ($type == 'local') {
-    if (function_exists('posix_getpwuid')) {
-        $user = posix_getpwuid(posix_geteuid())['name'];
-    } elseif (!empty($_SERVER['USER'])) {
-        $user = $_SERVER['USER'];
-    } else {
-        $user = '';
-    }
-    $pass = '';
-    $host = 'localhost';
-    $port = 0;
-    $d_name = 'localhost';
+if ($instance->type !== 'local') {
+    $access->host = promptUser('Host name');
+    $access->user = promptUser('User');
+    $access->password = $instance->type == 'ftp' ? promptPassword() : '';
+    $port = promptUser('Port number', ($instance->type == 'ssh') ? 22 : 21);
+} else {
+    $access->host = 'localhost';
+    $access->user = $discovery->detectUser();
 }
 
-$name = $contact = $webroot = $tempdir = $weburl = '';
+$instance->weburl = promptUser('Web URL', $discovery->detectWeburl());
+$instance->name = promptUser('Instance name', $discovery->detectName());
+$instance->contact = strtolower(promptUser('Contact email'));
 
-$name = promptUser('Instance name', $d_name);
-$contact = strtolower(promptUser('Contact email'));
-
-$instance = new Instance;
-$instance->name = $name;
-$instance->contact = $contact;
-$instance->webroot = rtrim($webroot, '/');
-$instance->weburl = rtrim($weburl, '/');
-$instance->tempdir = rtrim($tempdir, '/');
-
-if ($type == 'ftp') {
-    $webroot = promptUser('Web root', "/home/$user/public_html");
-    $weburl = promptUser('Web URL', "http://$host");
-
-    $instance->webroot = rtrim($webroot, '/');
-    $instance->weburl = rtrim($weburl, '/');
+if(!$access->firstConnect()) {
+    error('Failed to setup access');
 }
 
 $instance->save();
+$access->save();
 echo color("Instance information saved.\n", 'green');
 
-$access = $instance->registerAccessMethod($type, $host, $user, $pass, $port);
+info("Running on " . $discovery->detectDistro());
+$instance->webroot = promptUser('Web root', $discovery->detectWebroot());
+$instance->tempdir = promptUser('Working directory', TRIM_TEMP);
 
-if (! $access) {
-    $instance->delete();
-    echo color("Set-up failure. Instance removed.\n", 'red');
-}
+list($backup_user, $backup_group, $backup_perm) = $discovery->detectBackupPerm();
+$instance->backup_user = promptUser('Backup owner', $backup_user);
+$instance->backup_group = promptUser('Backup group', $backup_group);
+$instance->backup_perm = intval(promptUser('Backup file permissions', decoct($backup_perm)), 8);
 
-info('Detecting remote configuration.');
-if (! $instance->detectSVN()) { 
-    die(color("Subversion not detected on the remote server\n", 'red'));
-}
-
-if (! $instance->detectPHP()) {
-    die(color("PHP Interpreter could not be found on remote host.\n", 'red'));
-}
-else {
-    if ($instance->phpversion < 50300) {
-        die(color("PHP Interpreter version is less than 5.3.\n", 'red'));
-    }
-}
-
-$d_linux = $instance->detectDistribution();
-info("You are running : $d_linux");
-
-switch ($d_linux) {
-case "ClearOS":
-    $backup_user = @posix_getpwuid(posix_geteuid())['name'];
-    $backup_group = 'allusers';
-    $backup_perm = 02770;
-    $host = preg_replace("/[\\\\\/?%*:|\"<>]+/", '-', $instance->name);
-    $d_webroot = ($user == 'root' || $user == 'apache') ?
-        "/var/www/virtual/{$host}/html/" : "/home/$user/public_html/";
-    break;
-case "Windows":
-	$backup_user = $backup_group = "Administrator";
-	$backup_perm = 0750;
-	$d_webroot = 'D:\\www\\';
-	break;
-default:
-    $backup_user = @posix_getpwuid(posix_geteuid())['name'];
-    $backup_group = @posix_getgrgid(posix_getegid())['name'];
-    $backup_perm = 0750;
-    $d_webroot = ($user == 'root' || $user == 'apache') ?
-        '/var/www/html/' : "/home/$user/public_html/";
-}
-
-if ($type != 'ftp') {
-    $webroot = promptUser('Web root', $d_webroot);
-    $weburl = promptUser('Web URL', "http://$host");
-}
-$tempdir = promptUser('Working directory', TRIM_TEMP);
+$instance->phpexec = $discovery->detectPHP();
+$instance->phpversion = $discovery->detectPHPVersion();
 
 if ($access instanceof ShellPrompt) {
-    if(!is_dir($tempdir))
-		mkdir($tempdir, 0777, true);
+    $access->shellExec('mkdir -m777 -p ' . escapeshellarg($instance->tempdir));
 } else
     echo die(color("Shell access is required to create the working directory. You will need to create it manually.\n", 'yellow'));
-
-$backup_user = promptUser('Backup owner', $backup_user);
-$backup_group = promptUser('Backup group', $backup_group);
-$backup_perm = promptUser('Backup file permissions', decoct($backup_perm));
-
-$instance->weburl = rtrim($weburl, '/');
-$instance->webroot = rtrim($webroot, '/');
-$instance->tempdir = rtrim($tempdir, '/');
-
-$instance->backup_user = trim($backup_user);
-$instance->backup_group = trim($backup_group);
-$instance->backup_perm = octdec($backup_perm);
 
 $instance->save();
 echo color("Instance information saved.\n", 'green');
