f54f8dd29bf82691885fe90a7ae968bc05fb5e7b
pom2ter <pom2ter@users.sourceforge.net>
2018-09-07 02:18:14 +0000

    [FIX] trim: using the web ui to do backups crashed on a mac
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trim@67374 b456876b-0849-0410-b77d-98878d47e9d5

--- www/scripts/backup.php
+++ www/scripts/backup.php
@@ -10,10 +10,40 @@ ob_end_clean();
 if (isset($_POST['id'])){
     if ( $instance = Instance::getInstance( (int) $_POST['id'] ) ) {
         info('Checking permissions before backup...');
-        $instance->getApplication()->fixPermissions();
-        info('Check done!');
-        info('Backing up instance...');
-        $instance->backup();
+        $app = $instance->getApplication();
+        $app->fixPermissions();
+
+        $backup = new Backup($instance);
+
+        if ($instance->detectDistribution() === 'ClearOS') {
+            $backup->setArchiveSymlink(dirname($instance->webroot) . '/backup');
+        }
+
+//        $access = $backup->getAccess();
+        $backupDir = rtrim(BACKUP_FOLDER, '/') . "/{$instance->id}-{$instance->name}";
+        $archiveDir = rtrim(ARCHIVE_FOLDER, '/') . "/{$instance->id}-{$instance->name}";
+        $app->removeTemporaryFiles();
+
+        // the extrabackups query returns an error on a mac
+        $targets = $backup->getTargetDirectories();
+        foreach( $targets as $key=>$val ) {
+            if(strpos($val[1], 'command not found')) unset($targets[$key]);
+        }
+
+        info('Downloading files locally...');
+        $copyResult = $backup->copyDirectories($targets, $backupDir);
+
+        info('Creating manifest...');
+        $backup->createManifest($copyResult, $backupDir);
+
+        info('Creating database dump...');
+        $backup->createDatabaseDump($app, $backupDir);
+
+        info('Creating archive...');
+        $backup->createArchive($archiveDir, $backupDir);
+
+        // info('Backing up instance...');
+//        $instance->backup();
 //        perform_archive_cleanup($instance->id, $instance->name);
     } else {
         die( "Unknown instance." );
