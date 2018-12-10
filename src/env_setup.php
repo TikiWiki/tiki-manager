TikiWiki/tiki-manager

commit 6aa88601e2bc9da141da698788605336484921c9

[ENH] Improve make check/watch to detect instance revision mismatches
- Fix issues when a trim instance is managed manually causing check/watch to output huge file changes.

git-svn-id: svn+ssh://svn.code.sf.net/p/tikiwiki/code/trim@68467 b456876b-0849-0410-b77d-98878d47e9d5

src/env_setup.php
@@ -179,6 +179,7 @@ switch ($version) {
             instance_id INTEGER,
             type VARCHAR(10),
             branch VARCHAR(50),
+            revision VARCHAR(25),
             date VARCHAR(25)
         );
 
@@ -248,6 +249,11 @@ switch ($version) {
                 ON CONFLICT REPLACE
         );
         UPDATE info SET value = '5' WHERE name = 'version';
+    ");
+case 5:
+	$db->exec("
+        ALTER TABLE version ADD COLUMN revision VARCHAR(25);
+        UPDATE info SET value = '6' WHERE name = 'version';
     ");
 }
 

scripts/check.php
@@ -30,7 +30,10 @@ if (empty($selection)) {
     }
 }
 
+/** @var Instance $instance */
 foreach ($selection as $instance) {
+
+    /** @var Version $version */
     $version = $instance->getLatestVersion();
 
     if (! $version) {
@@ -41,12 +44,50 @@ foreach ($selection as $instance) {
 
     info("Checking instance: {$instance->name}");
 
-    if ($version->hasChecksums()) {
-        handleCheckResult($instance, $version, $version->performCheck($instance));
-    } else {
-        $input = '';
-        $values = ['current', 'source', 'skip'];
+	$versionRevision = $version->revision;
+	$tikiRevision = $instance->getRevision();
+
+    if (! empty($versionRevision) && $versionRevision == $tikiRevision && $version->hasChecksums()) {
+		handleCheckResult($instance, $version, $version->performCheck($instance));
+		continue;
+	}
+
+    $fetchChecksum = false;
+
+	if (empty($versionRevision)) {
+        warning('No revision detected for instance.');
+        $fetchChecksum = true;
+	}
+
+	if (!empty($versionRevision) && $versionRevision != $tikiRevision) {
+        warning('Revision mismatch between trim version and instance.');
+        $fetchChecksum = true;
+    }
+
+    if (empty($trimInstanceRevision) || $trimInstanceRevision != $tikiRevision) {
+        warning('It is recommended to fetch new checksum information.');
+        $fetchChecksum = true;
+	}
+
+    if (! $version->hasChecksums()) {
         warning('No checksums exist.');
+        $fetchChecksum = true;
+    }
+
+    if ($fetchChecksum) {
+
+        // Create a new version
+        $version = $instance->createVersion();
+        /** @var Application_Tiki $app */
+        $app = $instance->getApplication();
+        $version->type = $app->getInstallType();
+        $version->branch = $app->getBranch();
+        $version->date = date('Y-m-d');
+        $version->save();
+
+        $input = '';
+        $values = array('current', 'source', 'skip');
+
         echo "What do you want to do?\n";
         echo "  current - Use the files currently online for checksum.\n";
         echo "   source - Get checksums from repository (best option).\n";

scripts/watch.php
@@ -33,6 +33,26 @@ foreach ($instances as $instance) {
         continue;
     }
 
+    $versionError = false;
+    $versionRevision = $version->revision;
+    $tikiRevision = $instance->getRevision();
+
+    if (empty($versionRevision)) {
+        $log .= "No revision detected for {$instance->name}\n";
+        $versionError = true;
+    } elseif ($versionRevision != $tikiRevision) {
+        $log .= "Check {$instance->name} version conflict\n";
+        $log .= "Expected revision {$versionRevision}, found revision {$tikiRevision} on instance.\n";
+        $versionError = true;
+    }
+
+    if ($versionError) {
+        $log .= "Fix this error with TRIM by running \"make check\" and choose instance \"{$instance->id}\".";
+        $log .= "\n\n";
+
+        continue;
+    }
+
     if ($version->hasChecksums()) {
         $result = $version->performCheck($instance);
 

src/appinfo/tiki.php
@@ -52,6 +52,37 @@ class Application_Tiki extends Application
         }
     }
 
+    /**
+     * Get SVN revision information
+     *
+     * @param string|null $folder If valid folder or null it will collect the svn revision from the folder|instance webroot.
+     * @return int
+     */
+    public function getRevision($folder = null) {
+
+        $svnInfo = '';
+
+        if (file_exists($folder)) {
+            $svnInfo = `svn info $folder`;
+        }
+
+        if (is_null($folder)) {
+            $access = $this->instance->getBestAccess('scripting');
+            if ($access instanceof ShellPrompt && $access->hasExecutable('svn')) {
+                $host = $access->getHost();
+                $svnInterpreter = $access->getSVNPath();
+                $svnInfo = $host->runCommands("$svnInterpreter info {$this->instance->webroot}");
+            }
+        }
+
+        if (! empty($svnInfo)) {
+            preg_match('/(.*Rev:\s+)(.*)/', $svnInfo, $matches);
+            return $matches[2];
+        }
+
+        return 0;
+    }
+
     function fixPermissions()
     {
         $access = $this->instance->getBestAccess('scripting');

src/applicationlib.php
@@ -141,6 +141,17 @@ abstract class Application
     function performUpgrade(Instance $instance, $version, $abort_on_conflict = true)
     {
         $this->performActualUpgrade($version, $abort_on_conflict);
+
+        // Create a new version if process did not abort
+        $new = $instance->createVersion();
+        $new->type = $version->type;
+        $new->branch = $version->branch;
+        $new->date = $version->date;
+        $new->save();
+
+        info('Obtaining new checksum from source.');
+        $new->collectChecksumFromSource($instance);
+
     }
 
     function registerCurrentInstallation()

src/libs/trim/Instance.php
@@ -6,7 +6,7 @@
 
 define('SQL_SELECT_INSTANCE', "
 SELECT
-    i.instance_id id, i.name, i.contact, i.webroot, i.weburl, i.tempdir, i.phpexec, i.app, a.type, v.branch
+    i.instance_id id, i.name, i.contact, i.webroot, i.weburl, i.tempdir, i.phpexec, i.app, a.type, v.branch, v.revision
 FROM
     instance i
 INNER JOIN access a
@@ -17,7 +17,7 @@ LEFT JOIN
 
 define('SQL_SELECT_INSTANCE_BY_ID', "
 SELECT
-    i.instance_id id, i.name, i.contact, i.webroot, i.weburl, i.tempdir, i.phpexec, i.app, a.type, v.branch
+    i.instance_id id, i.name, i.contact, i.webroot, i.weburl, i.tempdir, i.phpexec, i.app, a.type, v.branch, v.revision
 FROM
     instance i
 INNER JOIN access a
@@ -49,7 +49,7 @@ WHERE
 
 define('SQL_SELECT_LATEST_VERSION', "
 SELECT
-    version_id id, instance_id, type, branch, date
+    version_id id, instance_id, type, branch, date, revision
 FROM
     version
 WHERE
@@ -671,5 +671,19 @@ class Instance
         if (isset($this->$name)) {
             return $this->$name;
         }
+    }
+
+    /**
+     * Get instance application revision
+     *
+     * @return mixed
+     */
+    public function getRevision() {
+
+        if ($this->app == 'tiki') {
+            return $this->getApplication()->getRevision();
+        }
+
+        return null;
     }
 }

src/libs/trim/Version.php
@@ -9,15 +9,16 @@ class Version
     const SQL_INSERT_VERSION = "
         INSERT OR REPLACE INTO
             version
-            (version_id, instance_id, type, branch, date)
+            (version_id, instance_id, type, branch, revision, date)
         VALUES
-            (:id, :instance, :type, :branch, :date)
+            (:id, :instance, :type, :branch, :revision, :date)
         ;";
 
     private $id;
     private $instance;
     public $type;
     public $branch;
+    public $revision;
     public $date;
     public $audit;
 
@@ -43,6 +44,7 @@ class Version
             ':instance' => $this->instance,
             ':type' => $this->type,
             ':branch' => $this->branch,
+            ':revision' => $this->revision,
             ':date' => $this->date,
         ];
 
@@ -60,8 +62,12 @@ class Version
         return Instance::getInstance($this->instance);
     }
 
-    function getBranch()
-    {
+	function getRevision()
+	{
+		return $this->revision;
+	}
+
+    function getBranch() {
         return $this->branch;
     }
 
@@ -100,11 +106,21 @@ class Version
     {
         $app = $instance->getApplication();
         $result = Audit_Checksum::checksumSource($this, $app);
+
+        // Update revision information (requires cache folder created and populated in checksumSource)
+        $folder = cache_folder($app, $this);
+        $this->revision = $app->getRevision($folder);
+        $this->save();
+
         return Audit_Checksum::saveChecksums($this->id, $result);
     }
 
     function collectChecksumFromInstance(Instance $instance)
     {
+        // Update revision information
+        $this->revision = $instance->getRevision();
+        $this->save();
+
         $access = $instance->getBestAccess('scripting');
         $folder = $instance->webroot;
         $result = $instance->type === 'local'

commit 58c3084e21dafa5ceae8ae1e69669e491741fda7

[FIX] Fix issue with duplicate revision column when creating a trim.db introduced in r68467
git-svn-id: svn+ssh://svn.code.sf.net/p/tikiwiki/code/trim@68503 b456876b-0849-0410-b77d-98878d47e9d5

src/env_setup.php
@@ -179,7 +179,6 @@ switch ($version) {
             instance_id INTEGER,
             type VARCHAR(10),
             branch VARCHAR(50),
-            revision VARCHAR(25),
             date VARCHAR(25)
         );
 