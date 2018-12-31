4fb7f75a939735a6096cd4a726f1875cc2334798
Jorge Sá Pereira <jorgesapereira@gmail.com>
2018-12-31 19:19:08 +0000

    [NEW] Add database:delete command

--- tiki-manager.php
+++ tiki-manager.php
@@ -37,5 +37,6 @@ $application->add(new \TikiManager\Command\ResetManagerCommand());
 $application->add(new \TikiManager\Command\ReportManagerCommand());
 
 $application->add(new \TikiManager\Command\ViewDatabaseCommand());
+$application->add(new \TikiManager\Command\DeleteDatabaseCommand());
 
 $application->run();
