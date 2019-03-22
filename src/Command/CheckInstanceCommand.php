5d5e59d364d28bdd8f69315de07d96d95e2ec536
Jorge Sá Pereira <me@jorgesapereira.com>
2019-03-22 18:14:39 +0000

    Add option to collect checksum using instance:check command

--- src/Command/CheckInstanceCommand.php
+++ src/Command/CheckInstanceCommand.php
@@ -2,8 +2,10 @@
 
 namespace TikiManager\Command;
 
+use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Question\ChoiceQuestion;
@@ -16,29 +18,41 @@ class CheckInstanceCommand extends Command
         $this
             ->setName('instance:check')
             ->setDescription('Check instance')
-            ->setHelp('This command allows you to check an instance');
+            ->setHelp('This command allows you to check an instance')
+            ->addOption('instances', null, InputOption::VALUE_OPTIONAL, 'List of instance IDs to be checked, separated by comma (,)')
+            ->addOption('update-from', null, InputOption::VALUE_OPTIONAL, 'Action related to how checksums are performed. Accepted values - current or source');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
+        $helper = $this->getHelper('question');
 
         $instances = CommandHelper::getInstances('all', true);
         $instancesInfo = CommandHelper::getInstancesInfo($instances);
+
         if (isset($instancesInfo)) {
-            $io->newLine();
-            $renderResult = CommandHelper::renderInstancesTable($output, $instancesInfo);
+            $instancesOption = $input->getOption('instances');
 
-            $io->newLine();
-            $output->writeln('<comment>In case you want to check more than one instance, please use a comma (,) between the values</comment>');
+            if (empty($instancesOption)) {
+                $io->newLine();
+                $renderResult = CommandHelper::renderInstancesTable($output, $instancesInfo);
 
-            $helper = $this->getHelper('question');
-            $question = CommandHelper::getQuestion('Which instance(s) do you want to check', null, '?');
-            $question->setValidator(function ($answer) use ($instances) {
-                return CommandHelper::validateInstanceSelection($answer, $instances);
-            });
+                $io->newLine();
+                $output->writeln('<comment>In case you want to check more than one instance, please use a comma (,) between the values</comment>');
 
-            $selectedInstances = $helper->ask($input, $output, $question);
+                $question = CommandHelper::getQuestion('Which instance(s) do you want to check', null, '?');
+                $question->setValidator(function ($answer) use ($instances) {
+                    return CommandHelper::validateInstanceSelection($answer, $instances);
+                });
+
+                $selectedInstances = $helper->ask($input, $output, $question);
+            } else {
+                CommandHelper::validateInstanceSelection($instancesOption, $instances);
+                $instancesOption = explode(',', $instancesOption);
+                $selectedInstances = array_intersect_key($instances, array_flip($instancesOption));
+            }
+
             foreach ($selectedInstances as $instance) {
                 $version = $instance->getLatestVersion();
 
@@ -89,18 +103,27 @@ class CheckInstanceCommand extends Command
                     $version->date = date('Y-m-d');
                     $version->save();
 
-                    $output->writeln('<comment>No checksums exist.</comment>');
-                    $io->newLine();
-                    CommandHelper::renderCheckOptionsAndActions($output);
-                    $io->newLine();
+                    $updateFromOption = $input->getOption('update-from');
+                    if (empty($updateFromOption)) {
+                        $output->writeln('<comment>No checksums exist.</comment>');
+                        $io->newLine();
+                        CommandHelper::renderCheckOptionsAndActions($output);
+                        $io->newLine();
 
-                    $question = new ChoiceQuestion(
-                        'Please select an option to apply:',
-                        ['current', 'source', 'skip'],
-                        null
-                    );
-                    $question->setErrorMessage('Option %s is invalid.');
-                    $option = $helper->ask($input, $output, $question);
+                        $question = new ChoiceQuestion(
+                            'Please select an option to apply:',
+                            ['current', 'source', 'skip'],
+                            null
+                        );
+                        $question->setErrorMessage('Option %s is invalid.');
+                        $option = $helper->ask($input, $output, $question);
+                    } else {
+                        if (! in_array($updateFromOption, ['current', 'source'])) {
+                            throw new InvalidArgumentException("Invalid value for option 'update-from'. Accepted values: current or source.");
+                        }
+
+                        $option = $updateFromOption;
+                    }
 
                     if ($option == 'skip') {
                         continue;
