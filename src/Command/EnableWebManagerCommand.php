2e24ef948f533b49fcfd057baee8ce6db8b0aabb
Jorge Sá Pereira <me@jorgesapereira.com>
2019-07-25 02:07:45 +0100

    [ENH] Update webmanager:command to accept arguments

--- src/Command/EnableWebManagerCommand.php
+++ src/Command/EnableWebManagerCommand.php
@@ -4,10 +4,9 @@ namespace TikiManager\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use TikiManager\Command\Helper\CommandHelper;
 
 class EnableWebManagerCommand extends Command
 {
@@ -16,24 +15,54 @@ class EnableWebManagerCommand extends Command
         $this
             ->setName('webmanager:enable')
             ->setDescription('Activate Tiki Manager web administration')
-            ->setHelp('This command allows you to enable a web interface for Tiki Manager');
+            ->setHelp('This command allows you to enable a web interface for Tiki Manager')
+            ->addOption(
+                'path',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'The folder to install tiki-manager'
+            )
+            ->addOption(
+                'username',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'The username to login'
+            )
+            ->addOption(
+                'password',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'The username\'s password'
+            )
+            ->addOption(
+                'restrict',
+                null,
+                InputOption::VALUE_NONE,
+                'Restrict WebManager access to localhost'
+            )
+            ->addOption(
+                'www-user',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'The apache user (set this if other than apache)'
+            )
+            ->addOption(
+                'www-group',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'The apache group (set this if other than apache)'
+            )
+            ->addOption(
+                'install',
+                null,
+                InputOption::VALUE_NONE,
+                'Proceed tiki-manager installation'
+            );
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function interact(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
-
-        if (function_exists('posix_getuid')) {
-            if (posix_getuid() != 0) {
-                $output->writeln('<error>You need to run this script as root to write to configuration files.</error>');
-                exit(-1);
-            }
-        } else {
-            $output->writeln('<error>PHP POSIX functions are not installed, install them and try again.</error>');
-            exit(-1);
-        }
-
-        $io->newLine();
 
         $output->writeln('Tiki Manager web administration files are located in the Tiki Manager directory. In order to
 make the interface available externally, the files will be copied to a web
@@ -48,69 +77,116 @@ For example, if your web root is /var/www/virtual/webtikimanager.example.com
     http://webtikimanager.example.com
 * You must have write access in /var/www/virtual
 
-Simple authentification will be used. However, it is possible to restrict
+Simple authentication will be used. However, it is possible to restrict
 access to the administration panel to local users (safer).');
 
         $io->newLine();
 
-        $helper = $this->getHelper('question');
-        $question = new ConfirmationQuestion('<comment>This will enable the Tiki Manager administration web panel.</comment>
-Continue with this action (y,n)? ', false);
+        if (!$install = $input->getOption('install')) {
+            $install = $io->confirm('This will enable the Tiki Manager administration web panel. Continue with this action?', false);
 
-        if (!$helper->ask($input, $output, $question)) {
+            if (!$install) {
+                exit(1);
+            }
+
+            $input->setOption('install', $install);
+        }
+
+        $path = $io->ask(
+            'WWW Tiki Manager directory (ex: /var/www/virtual/webtikimanager.example.com/html)',
+            getenv('WWW_PATH') ?: null,
+            function ($value) {
+                if (empty(trim($value))) {
+                    throw new \RuntimeException('Tiki Manager directory cannot be empty');
+                }
+                return $value;
+            }
+        );
+
+        $input->setOption('path', $path);
+
+        if (file_exists($path . '/config.php')) {
             return;
         }
 
-        $io->newLine();
+        if (!$input->getOption('username')) {
+            $username = $io->ask('Desired username');
+            $input->setOption('username', $username);
+        }
 
-        $question = CommandHelper::getQuestion(
-            'WWW Tiki Manager directory (ex: /var/www/virtual/webtikimanager.example.com/html)',
-            getenv('WWW_PATH') ?: null
-        );
-        $question->setValidator(function ($value) {
-            if (empty(trim($value))) {
-                throw new \RuntimeException('Tiki Manager directory cannot be empty');
-            }
-            return $value;
-        });
-        $webTrimDirectory = $helper->ask($input, $output, $question);
-        $cmd = 'cp -a www/. ' . $webTrimDirectory;
-        exec($cmd);
-
-        $owner = fileowner($webTrimDirectory . '/index.php');
-
-        if (!file_exists($webTrimDirectory . '/config.php')) {
-            $question = CommandHelper::getQuestion('Desired username');
-            $username = $helper->ask($input, $output, $question);
-
-            $question = CommandHelper::getQuestion('Desired password');
-            $question->setValidator(function ($value) {
+        if (!$input->getOption('password')) {
+            $password = $io->askHidden('Desired password', function ($value) {
                 if (empty(trim($value))) {
                     throw new \Exception('The password cannot be empty');
                 }
                 return $value;
             });
-            $question->setHidden(true);
-            $password = $helper->ask($input, $output, $question);
+            $input->setOption('password', $password);
+        }
 
-            $question = CommandHelper::getQuestion('Restrict use to localhost', 'no');
-            $question->setNormalizer(function ($value) {
-                return (strtolower($value{0}) == 'n') ? 'false' : 'true';
-            });
-            $restrict = $helper->ask($input, $output, $question);
+        if (!$input->getOption('restrict')) {
+            $restrict = $io->confirm('Restrict use to localhost', false);
+            $input->setOption('restrict', $restrict);
+        }
+    }
 
-            $tiki_manager_path = realpath(dirname(__FILE__) . '/../..');
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        if (function_exists('posix_getuid')) {
+            if (posix_getuid() != 0) {
+                throw new \RuntimeException('You need to run this script as root to write to configuration files.');
+            }
+        } else {
+            throw new \RuntimeException('PHP POSIX functions are not installed, install them and try again.');
+        }
+
+        if (!$input->getOption('install')) {
+            throw new \RuntimeException('Missing confirmation to proceed installation');
+        }
+
+        $webPath = $input->getOption('path') ?? $_ENV['WWW_PATH'];
+
+        if (empty(trim($webPath))) {
+            throw new \RuntimeException('Tiki Manager directory cannot be empty');
+        }
+
+        if (!is_dir($webPath)) {
+            throw new \RuntimeException('Tiki Manager Web path does not exist.');
+        }
+
+        $cmd = 'cp -a www/. ' . $webPath;
+        exec($cmd);
+
+        $owner = fileowner($webPath . '/index.php');
+
+        if (!file_exists($webPath . '/config.php')) {
+            $username = $input->getOption('username');
+
+            if (empty($username)) {
+                throw new \RuntimeException('Username value is missing');
+            }
+
+            $password = $input->getOption('password');
+            if (empty($password)) {
+                throw new \RuntimeException('Password value is missing');
+            }
+
+            $restrict = (int) $input->getOption('restrict');
+
+            $tikiManagerPath = realpath(dirname(__FILE__) . '/../..');
 
             $user = addslashes($username);
             $pass = addslashes($password);
 
-            file_put_contents($webTrimDirectory . '/config.php', <<<CONFIG
+            file_put_contents($webPath . '/config.php', <<<CONFIG
 <?php
 define('USERNAME', '$user');
 define('PASSWORD', '$pass');
 define('RESTRICT', $restrict);
 define('TIMEOUT', 0);
-define('TRIMPATH', '$tiki_manager_path');
+define('TRIMPATH', '$tikiManagerPath');
 define('THEME', 'default');
 define('TITLE', 'Tiki Manager Web Administration');
 CONFIG
@@ -130,10 +206,9 @@ CONFIG
         `chown $user:$group $data`;
         `chown $user:$group $backup`;
         `chown $user:$group $archive`;
-        `(rm -rf $webTrimDirectory/vendor && $composer install -d $webTrimDirectory)`;
-        `(chown -R $owner $webTrimDirectory/vendor)`;
+        `(rm -rf $webPath/vendor && $composer install -d $webPath)`;
+        `(chown -R $owner $webPath/vendor)`;
 
-        $output->writeln('<info>WWW Tiki Manager is now enabled.</info>');
-        $output->writeln('<info>Enjoy!</info>');
+        $io->success('WWW Tiki Manager is now enabled.');
     }
 }
