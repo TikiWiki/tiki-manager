9ec7f7b0452f8430a72b801ff9c0a8b987b461cf
Eric Ampire <ericampire.top@gmail.com>
2019-11-22 11:56:03 +0000

    [UDP] Change head.php

--- www/include/layout/head.php
+++ www/include/layout/head.php
@@ -8,7 +8,7 @@
         <title><?php echo (defined("TITLE")) ? TITLE :'Tiki Manager Web Administration ';  ?> : <?php echo $page_title; ?></title>
         <link href="vendor/twitter/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
         <link href="vendor/fortawesome/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-        <link href="themes/<?php echo THEME; ?>/css/<?php echo THEME; ?>.css" rel="stylesheet">
+        <link href="themes/<?php echo defined('THEME') ? THEME : 'default'; ?>/css/<?php echo defined('THEME') ? THEME : 'default'; ?>.css" rel="stylesheet">
     </head>
 
     <body>
