a271871c228b039cdec93e57661e2e25a5265c02
Jorge Sá Pereira <me@jorgesapereira.com>
2020-10-29 23:55:34 +0000

    [ENH] Display login failure in webmanager

--- www/include/login.php
+++ www/include/login.php
@@ -5,11 +5,15 @@ use TikiManager\Config\App;
 $info = App::get('info');
 $isLoginLocked = $info->isLoginLocked();
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && ! $isLoginLocked) {
+$invalidLogin = $_SESSION['invalid_login'] ?: false;
+unset($_SESSION['invalid_login']);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && !$isLoginLocked) {
     if ($_POST['user'] == USERNAME && $_POST['pass'] == PASSWORD) {
         $_SESSION['active'] = true;
         $info->resetLoginAttempts();
     } else {
+        $_SESSION['invalid_login'] = true;
         $info->incrementLoginAttempts();
     }
 
@@ -31,24 +35,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && ! $isLoginLocked) {
         <div class="row justify-content-center">
             <div class="col-md-8 col-lg-6">
                 <form method="post" action="<?php echo html($_SERVER['REQUEST_URI']) ?>">
-                    <?php if ($isLoginLocked) :?>
+                    <?php if ($isLoginLocked) : ?>
                         <div class="alert alert-danger" role="alert">
                             <h6>Too many invalid login attempts</h6>
                             <small>Login is temporarily disabled, please contact an administrator to unlock the login process.</small>
                         </div>
-                    <?php endif;?>
+                    <?php endif; ?>
+                    <?php if ($invalidLogin && !$isLoginLocked) : ?>
+                        <div class="alert alert-danger" role="alert">
+                            <h6>Login failed</h6>
+                            <small> Invalid username or password.</small>
+                        </div>
+                    <?php endif; ?>
                     <fieldset>
                         <div class="form-group">
                             <label for="username">Username:</label>
-                            <input id="username" class="form-control" type="text" name="user"/>
+                            <input id="username" class="form-control" type="text" name="user" />
                         </div>
                         <div class="form-group">
                             <label for="password">Password:</label>
-                            <input id="password" class="form-control" type="password" name="pass"/>
+                            <input id="password" class="form-control" type="password" name="pass" />
                         </div>
                         <div class="form-group center">
                             <button class="btn btn-primary" <?php echo $loginDisabled; ?> type="submit">
-                                Submit
+                                Log in
                             </button>
                         </div>
                     </fieldset>
