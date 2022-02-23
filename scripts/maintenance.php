8f9f0a9f5f72426b0fe2533865825b64d2526268
eutyche odimba <keutyche@gmail.com>
2022-02-23 12:30:34 +0100

    [ENH] change of start time from 12 hours to 24 hours

--- scripts/maintenance.php
+++ scripts/maintenance.php
@@ -18,7 +18,7 @@ date_timestamp_set($timestamp, filemtime(__FILE__));
 // Get the differences of both
 $interval = date_diff($timestamp, $updateTime);
 // Format the display with full date and time of start (timestamp of file)
-$dateStart = gmdate('Y-m-d h:i:s', $timestamp->getTimestamp());
+$dateStart = gmdate('Y-m-d G:i:s', $timestamp->getTimestamp());
 // Add the total number of days as hours
 $interval->h = $interval->h + ($interval->days * 24);
 // Format the display with usual hours, minutes, seconds (don't need the day, we hope so !)
