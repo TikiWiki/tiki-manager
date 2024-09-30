5202e67ba5566f1e09f9821f042a3ae7a756eb56
sandy-12 <sandeep.anandbhai@softstart.io>
2024-09-30 13:44:47 +0530

    Minor fix

--- src/Application/Instance.php
+++ src/Application/Instance.php
@@ -1288,6 +1288,22 @@ SQL;
         $this->getVersionControlSystem()->revert($this->webroot);
     }
 
+    /**
+     * Check if execution timeout is higher in tiki instance preference
+     *
+     * @param string $timeout
+     * @return string
+     */
+    private function getMaxExecTimeout($timeout) : string
+    {
+        $instanceExecTimeout = $this->getApplication()->getPref('allocate_time_unified_rebuild');
+        if ($instanceExecTimeout && (int) $instanceExecTimeout > (int) $timeout) {
+            return $instanceExecTimeout;
+        }
+
+        return $timeout;
+    }
+
     public function addOrUpdateInstanceTag(string $tagName, string $tagValue): bool
     {
         $params = [':id' => $this->id, ':tagname' => $tagName, ':tagvalue' => $tagValue];
@@ -1335,21 +1351,5 @@ SQL;
             }
         }
         return false;
-    }
-
-    /**
-     * Check if execution timeout is higher in tiki instance preference
-     *
-     * @param string $timeout
-     * @return string
-     */
-    private function getMaxExecTimeout($timeout) : string
-    {
-        $instanceExecTimeout = $this->getApplication()->getPref('allocate_time_unified_rebuild');
-        if ($instanceExecTimeout && (int) $instanceExecTimeout > (int) $timeout) {
-            return $instanceExecTimeout;
-        }
-
-        return $timeout;
     }
 }
