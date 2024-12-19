dc8188970735200a9365099b823548d68165b118
John Livingston <git@john-livingston.fr>
2024-12-19 16:36:19 +0100

    [FIX] instance:info: multiple instances selection.
    Parts of the code assumed we can select multiple instances (by using ','
    or 'all'), but in fact the code did not handle it well.

--- src/Command/InstanceInfoCommand.php
+++ src/Command/InstanceInfoCommand.php
@@ -20,7 +20,14 @@ class InstanceInfoCommand extends TikiManagerCommand
             ->setName('instance:info')
             ->setDescription('Show Tiki instance Info')
             ->setHelp('This command allows you to show Tiki instance information, and can get information on json format')
-            ->addOption('instance', 'i', InputOption::VALUE_REQUIRED, 'Sources instance')
+            ->addOption(
+                'instance',
+                'i',
+                InputOption::VALUE_REQUIRED,
+                'Instance from which you want to get some information (ID or name). '
+                .' You get informations about multiple instances by using a comma (,) separated list. '
+                .' You can get detailed information on all instance by using the "all" keyword.'
+            )
             ->addOption('format', null, InputOption::VALUE_OPTIONAL, 'Output format (table or json)', 'table');
     }
 
@@ -42,7 +49,13 @@ class InstanceInfoCommand extends TikiManagerCommand
             }
 
             CommandHelper::renderInstancesTable($output, $this->instancesInfo);
-            $answer = $this->io->ask('Which instance do you want to get info for', null, function ($answer) {
+
+            $this->io->newLine();
+            $output->writeln('<comment>In case you want to access more than one instance, please use a comma (,) between the values.</comment>');
+            $this->io->newLine();
+            $output->writeln('<comment>You can also get informations for all instance by using the "all" keyword.</comment>');
+
+            $answer = $this->io->ask('Which instance(s) do you want to get info for', null, function ($answer) {
                 $selectedInstances = CommandHelper::validateInstanceSelection($answer, $this->instances);
                 return implode(',', array_map(function ($elem) {
                     return $elem->getId();
@@ -60,15 +73,13 @@ class InstanceInfoCommand extends TikiManagerCommand
             return 0;
         }
         $instancesOption = $input->getOption('instance');
-        $filtered_instances = array_filter($this->instances, function ($instance) use ($instancesOption) {
-            return( $instance->id == $instancesOption || $instance->name === $instancesOption);
-        });
-        $selectedinstance = CommandHelper::getInstancesInfo($filtered_instances, true);
+        $filtered_instances = CommandHelper::validateInstanceSelection($instancesOption, $this->instances);
+        $selectedinstances = CommandHelper::getInstancesInfo($filtered_instances, true);
 
         if (strtolower($input->getOption('format')) == 'json') {
-            $output->writeln(json_encode($selectedinstance));
+            $output->writeln(json_encode($selectedinstances));
         } else {
-            CommandHelper::renderInstancesTable($output, $selectedinstance, true);
+            CommandHelper::renderInstancesTable($output, $selectedinstances, true);
         }
         return 0;
     }
